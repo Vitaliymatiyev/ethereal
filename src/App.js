b95b6f60288f521c0b3e22574c484812b1157ba8
Vitaliy Matiyev <vitaliymatiyev@Vitaliys-MacBook-Pro.local>
2024-01-21 16:20:18 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Cube from './Cube.jpeg';
+import './output.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+           Welcome to Ethereal!
+        </h1>
       </header>
+      <div>
+        <img src={Cube} alt= "Image display a building size lexicon cube size"/>
+        <p className="text-3xl font-bold underline"> Randome Text for now </p>
+      </div>
+
     </div>
   );
 }
