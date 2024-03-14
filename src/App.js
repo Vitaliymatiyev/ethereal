34edbdd56c9ec99ff8c8e43723a9e076c17e15d1
vmatiyev <vitaliy.matiyev@gmail.com>
2024-03-13 17:48:46 -0700

    Add header logo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import Cube from './Cube.jpeg';
+import etherIcon from './etherealWithIcon.png';
 import './output.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
+    <div>
+        <nav className='sticky top-0 z-50 p-2 bg-black'>
+          <img src={etherIcon} alt= "Ethereal Icon" className="object-contain h-[80px] w-[200px]"/>
+        </nav>
+        <h1 className='font-mono text-xl'>
            Welcome to Ethereal!
         </h1>
-      </header>
       <div>
         <img src={Cube} alt= "Image display a building size lexicon cube size"/>
         <p className="text-3xl font-bold underline"> Randome Text for now </p>
