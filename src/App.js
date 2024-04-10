13496857f8e7d69014c571b29a25f815fbbd030e
vmatiyev <vitaliy.matiyev@gmail.com>
2024-04-10 16:49:56 -0700

    Adding body on page and image

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import './App.css';
-import Cube from './Cube.jpeg';
-import etherIcon from './etherealWithIcon.png';
+import Ethereal from './EtherealNameAndLogo.png';
+import etherIcon from './EtherealLogo.png';
 import './output.css';
 
 function App() {
   return (
     <div>
         <nav className='sticky top-0 z-50 p-2 bg-black'>
-          <img src={etherIcon} alt= "Ethereal Icon" className="object-contain h-[80px] w-[200px]"/>
+          <img src={etherIcon} alt= "Ethereal Icon" className="object-contain h-[80px] w-[80px]"/>
         </nav>
-        <h1 className='font-mono text-xl'>
-           Welcome to Ethereal!
-        </h1>
-      <div>
-        <img src={Cube} alt= "Image display a building size lexicon cube size"/>
-        <p className="text-3xl font-bold underline"> Randome Text for now </p>
+
+      <div className= "flex justify-center mt-10">
+        <div className="max-w-[1200px] flex flex-col items-center">
+          <img src={Ethereal} alt= "Image displays Ethereal name with Icon" className=" w-[700px]"/>
+          <p className="text-xl text-center text-white"> The Ethereal Guild is an elite assembly of adventurers bound by their mastery over the five elements: Earth, Air, Fire, Water, and the elusive fifth element, Aether. </p>
+        </div>
+
       </div>
 
     </div>
